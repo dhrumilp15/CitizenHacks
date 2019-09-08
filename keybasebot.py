698022492968a28eda9761e1253065e2fdfb7f3d
dhrumilp15 <dhrumilp15@gmail.com>
2019-09-08 04:09:53 -0400

    works now

--- keybasebot.py
+++ keybasebot.py
@@ -1,104 +1,40 @@
-# from flask import redirect, render_template, request, session
-import pykeybasebot
+#!/usr/bin/env python3
+
 import asyncio
-import functools
 import logging
 import os
 import sys
+
 import pykeybasebot.types.chat1 as chat1
+from pykeybasebot import Bot
 
-logging.basicConfig(level = logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
-asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+if "win32" in sys.platform:
+    # Windows specific event-loop policy
+    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
 
-def force_async(fn):
-    from concurrent.futures import ThreadPoolExecutor
-    import asyncio
 
-    pool = ThreadPoolExecutor()
+class Handler:
+    async def __call__(self, bot, event):
+        channel = event.msg.channel
+        if event.msg.content.type_name == chat1.MessageTypeStrings.TEXT.value:
+            bot.chat.send(channel, "Could you send your medical data into the chat?")
+        if event.msg.content.type_name == chat1.MessageTypeStrings.ATTACHMENT.value:
+            print("conversation: {} messageId: {} filename: {}".format(channel.name, event.msg.id, event.msg.content))
+            filename = event.msg.content.attachment.object.filename
+            os.system("keybase chat download {channel} {attachmentId} -o {filename}".format(channel = channel.name, attachmentId = int(event.msg.id), filename = filename))
+            os.system("keybase fs mv {filename} /keybase/private/{doc},{patientName}".format(filename = filename, doc = bot.username, patientName = {channel.name.replace(bot.username + ",", "")}))
+            os.system("keybase fs rm -f {filename}".format(filename = filename))
 
-    @functools.wraps(fn)
-    def wrapper(*args, **kwargs):
-        future = pool.submit(fn, *args, **kwargs)
-        return asyncio.wrap_future(future)
+listen_options = {
+    "filter-channels": [
+        {"name": "dhrumilp15,sigilwen"}
+    ]
+}
 
-    return wrapper
-
-# class Handler:
-#     async def __call__(self, bot, event):
-#         if event.msg.content.type_name != chat1.MessageTypeStrings.TEXT.value:
-#             return
-#         await docbot.sharepatientdata(event.msg.content.text.body)
-        
-class DocBot:
-    def __init__(self, botname, paperkey, channels):
-        self.botname = botname
-        self.paperkey = paperkey
-        self.channels = channels
-        self._commands = []
-        self._bot = None
-        self.hcp = 'dhrumilp15'
-
-    def run(self):
-        self._bot = pykeybasebot.Bot(
-            username=self.botname, paperkey=self.paperkey, handler=self.command_handler
-        )
-        # bot_channels = []
-        # for channel in self.channels:
-        #     team, topic = channel.split("#")
-        #     bot_channels.append(
-        #         {"name": team, "topic_name": topic, "members_type": "team"}
-        #     )
-        listen_options = {"filter-channels": "dhrumilp15,sigilwen"}
-        asyncio.run(self._bot.start(listen_options))
-
-    def add_command(self, trigger_func, command_func):
-        async_command = force_async(command_func)
-        self._commands.append(
-            {"trigger_func": trigger_func, "command_func": async_command}
-        )
-
-    def reply(self, event, message):
-        channel = event.msg.channel  # format the channel for sending
-        # make a new loop to run the async bot.chat.send method in what is currently
-        # a synchronous context
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(self._bot.chat.send(channel, message))
-
-    async def command_handler(self, bot, event):
-        for possibility in self._commands:
-            if possibility["trigger_func"](self, event):
-                return await possibility["command_func"](self, event)
-    
-    async def sharepatientdata(self, patient):
-        # Technically don't need to force because folder should be empty but just in case
-        os.system("keybase fs cp -r -f /keybase/private/{patient} /keybase/private/{patient},{hcp}".format(patient = patient, hcp = self.hcp))
-    
-    async def updatepatientsdata(self,patient):
-        os.system("keybase fs cp -r -f /keybase/private/{hcp} /keybase/private/{hcp},{patient}".format(hcp = self.hcp, patient = patient))
-    
-    async def deletepatientdata(self):
-        os.system("keybase fs rm -r -f /keybase/private/{hcp}".format(hcp = self.hcp))
-
-docbot = DocBot(
-    botname = "dhrumilp15",
-    paperkey = "seek ethics twelve federal garment hockey vapor local vacuum bleak employ egg soap",
-    channels = ["dhrumilp15,sigilwen"]
+bot = Bot(
+    username="dhrumilp15", paperkey="holiday maid indoor dial sword leisure limit spend connect cheese round slot hat", handler=Handler()
 )
-def patientGreeting(bot, event):
-    if event.msg.content.type_name != chat1.MessageTypeStrings.TEXT.value:
-        return False
-    return event.msg.content.text.body == "I'm at the doctor's"
 
-def askHistory(bot, event):
-    if event.msg.content.type_name != chat1.MessageTypeStrings.TEXT.value:
-        return False
-    bot.reply(event, "Could you please send your medical history in the chat.")
-
-docbot.add_command(patientGreeting, askHistory)
-
-def gethistory(bot, event):
-    if event.msg.content.type_name
-
-docbot.run()
+asyncio.run(bot.start(listen_options))
